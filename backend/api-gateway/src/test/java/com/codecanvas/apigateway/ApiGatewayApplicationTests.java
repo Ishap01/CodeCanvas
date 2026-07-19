ead9891ac58901d2812b9fbca91ac2755fcd2ca5
Isha Patil <ishaa0182@gmail.com>
2026-07-19 16:13:43 +0530

    Setup API Gateway and Eureka Integration

---
+++ backend/api-gateway/src/test/java/com/codecanvas/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecanvas.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
